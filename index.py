e48ceec150ee0862709094b34a7a63f38b0424fe
BryCris15 <bcmalla@uce.edu.ec>
2025-05-28 00:05:58 -0500

    Eliminar vercel-wsgi y usar Flask directamente

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from vercel_wsgi import handle_request
 
 app = Flask(__name__)
 
@@ -7,6 +6,5 @@ app = Flask(__name__)
 def home():
     return "¿Sabías que aprender en red distribuye el conocimiento? ¡Tú también puedes hacerlo!"
 
-# Esta es la función que Vercel usará para ejecutar tu app
-def handler(environ, start_response):
-    return handle_request(app, environ, start_response)
+# Solo expón la app, sin handler extra
+
